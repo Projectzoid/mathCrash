6a13799e7022cafffc15a8bf9fad92400796db51
Anirud Lappathi <anirud.lappathi@gmail.com>
2022-05-29 17:39:12 -0700

    powerRule.py made

---
+++ powerRule.py
@@ -0,0 +1,12 @@
+from manim import * 
+
+config.frame_width = 37.9391100703
+config.frame_height = 32
+
+class Title(Scene):
+    def construct(self):
+        mathCrash = MarkupText(
+            f'math<span fgcolor="{WHITE}">Crash</span>', color=BLUE
+            text = 
+        )
+        self.add(mathCrash)
