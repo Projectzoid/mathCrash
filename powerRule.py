93bab616ce4d9261f757fb31c574c682c23d6b96
Anirud Lappathi <anirud.lappathi@gmail.com>
2022-06-10 00:43:57 -0700

    new animations

--- powerRule.py
+++ powerRule.py
@@ -4,7 +4,7 @@ from sympy import *
 #config.frame_width = 37.9391100703
 #config.frame_height = 32
 
-class PowerRule(MovingCameraScene):
+class PowerRule(Scene):
     def construct(self):
         mathCrash = MarkupText(
             f'math<span fgcolor="{WHITE}">Crash</span>', color=BLUE
@@ -24,28 +24,15 @@ class PowerRule(MovingCameraScene):
         self.wait(3)
         self.play(ReplacementTransform(mathCrash, topic), FadeOut(section))
         self.wait(5)
+        self.clear()
 
-        #equation1 = MathTex("y", "=", "x^2")
-
-        #self.add(equation1)
-        #self.play(self.camera.frame.animate.move_to(equation1[0]).set(width=equation1[0].width*2))
-        #self.wait(1)
-        #self.play(self.camera.frame.animate.move_to(equation1[2]).set(width=equation1[2].width*2))
-        
-
-        axes = Axes(
-            x_range=[-5, 5],
-            y_range=[-5, 5],
-            axis_config={"color": BLUE},
+        t1 = MarkupText(
+            f'<span fgcolor="{WHITE}">What is a</span> derivative<span fgcolor="{WHITE}">?</span> ', color=RED
         )
 
-        graph1 = axes.get_graph(lambda x: x**2, RED)
-        graph2 = axes.get_graph(lambda x: x*2, GREEN)
-        graph_label1 = axes.get_graph_label(graph1, label='x^{2}')
-        graph_label2 = axes.get_graph_label(graph2, label='2x')
+        self.play(Write(t1))
+        self.wait(3)
 
-        self.play(Create(axes), Create(graph1), Write(graph_label1))
-        self.wait(5)
-        self.play(Create(graph2), Write(graph_label2))
+        
       
   
