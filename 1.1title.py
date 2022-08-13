a2f8efe1931204e801d04331e2312a8832b6b241
Anirud Lappathi <anirud.lappathi@gmail.com>
2022-08-13 13:06:40 -0700

    finished video

---
+++ 1.1title.py
@@ -0,0 +1,69 @@
+from manim import *
+from sympy import * 
+
+#config.frame_width = 37.9391100703
+#config.frame_height = 32
+
+class LimitsTitle(Scene):
+    def construct(self):
+        mathCrash = MarkupText(
+            f'math<span fgcolor="{WHITE}">Crash</span>', color=BLUE
+        )
+
+        section = Text(
+            "Section 1.1", color=WHITE
+        )
+
+        topic = Text(
+            f'What is a limit? \nHow do you determine if a limit exists \nHow do you graphically solve for a limit', color=WHITE
+        )
+
+        section.next_to(mathCrash, DOWN)
+        
+        self.play(Write(mathCrash), Write(section), run_time = 1)
+        self.wait(3)
+        self.play(ReplacementTransform(mathCrash, topic), FadeOut(section), run_time = 0.5)
+
+        self.play(ApplyWave(
+            topic[0:16],
+            direction=UP,
+            time_width=0.5,
+            amplitude=0.1
+        ))
+
+        self.wait()
+
+        self.play(ApplyWave(
+            topic[17:55],
+            direction=UP,
+            time_width=0.5,
+            amplitude=0.1
+        ))
+
+        self.wait()
+
+        self.play(ApplyWave(
+            topic[56:96],
+            direction=UP,
+            time_width=0.5,
+            amplitude=0.1
+        ))
+    
+        self.wait(3)
+
+        t1 = MarkupText(
+            f'<span fgcolor="{WHITE}">What is a</span> limit<span fgcolor="{WHITE}">?</span> ', color=RED
+        )
+
+        self.play(ReplacementTransform(topic, t1), run_time = 1)
+        self.play(ApplyWave(
+            t1,
+            direction=UP,
+            time_width=0.5,
+            amplitude=0.1
+        ))
+        self.wait(3)
+
+  
+        
+      
